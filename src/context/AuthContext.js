4b9616772f98979b2ff2c12e18a3dbcb6ba88fb0
David Olowomeye <olowomeyedavid242@gmail.com>
2023-01-17 17:16:34 +0100

    Icluded the react context hook.

---
+++ src/context/AuthContext.js
@@ -0,0 +1,14 @@
+import { createContext, useReducer } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    
+
+
+    return (
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    )
+}
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -11,7 +11,7 @@ export default function Signup() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, displayName);
+    // console.log(email, password, displayName);
     signup(email, password, displayName);
   };
 
